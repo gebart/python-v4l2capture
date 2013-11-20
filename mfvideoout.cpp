c6db39e78d94a802f33e329e6c41313515edd6f4
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-11-20 14:29:28 +0000

    Initialise com

--- mfvideoout.cpp
+++ mfvideoout.cpp
@@ -1,17 +1,26 @@
 
 #include "mfvideoout.h"
+#include <mfapi.h>
+#include <Mferror.h>
 
 //http://msdn.microsoft.com/en-us/library/windows/desktop/ms700134%28v=vs.85%29.aspx
 
-
 MfVideoOut::MfVideoOut(const char *devName) : Base_Video_Out()
 {
+	HRESULT hr = MFStartup(MF_VERSION);
+	if(!SUCCEEDED(hr))
+		throw std::runtime_error("Media foundation startup failed");
 
+	hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+	if(hr == RPC_E_CHANGED_MODE)
+		throw std::runtime_error("CoInitializeEx failed");
 }
 
 MfVideoOut::~MfVideoOut()
 {
+	MFShutdown();
 
+	CoUninitialize();
 }
 
 void MfVideoOut::SendFrame(const char *imgIn, unsigned imgLen, const char *pxFmt, int width, int height)
