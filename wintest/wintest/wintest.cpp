74b8b9956948543a94839d85ec6c4f3513db36b9
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-11-20 14:26:11 +0000

    Add project to test windows video out

---
+++ wintest/wintest/wintest.cpp
@@ -0,0 +1,13 @@
+// wintest.cpp : Defines the entry point for the console application.
+//
+
+//#include "stdafx.h"
+#include "../../mfvideoout.h"
+
+int main(int argc, char* argv[])
+{
+	class MfVideoOut mfVideoOut("test");
+
+	return 0;
+}
+
