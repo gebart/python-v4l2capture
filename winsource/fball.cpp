7d7b9c88cf990b027276121dc0320c33b00dd4a7
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-11-22 10:20:09 +0000

    Video source is registered

--- winsource/fball.cpp
+++ winsource/fball.cpp
@@ -18,6 +18,10 @@
 
 // Setup data
 
+#define CreateComObject(clsid, iid, var) CoCreateInstance( clsid, NULL, CLSCTX_INPROC_SERVER, iid, (void **)&var);
+STDAPI AMovieSetupRegisterServer( CLSID   clsServer, LPCWSTR szDescription, LPCWSTR szFileName, LPCWSTR szThreadingModel = L"Both", LPCWSTR szServerType     = L"InprocServer32" );
+STDAPI AMovieSetupUnregisterServer( CLSID clsServer );
+
 const AMOVIESETUP_MEDIATYPE sudOpPinTypes =
 {
     &MEDIATYPE_Video,       // Major type
@@ -65,6 +69,61 @@ int g_cTemplates = sizeof(g_Templates) / sizeof(g_Templates[0]);
 //
 ////////////////////////////////////////////////////////////////////////
 
+STDAPI RegisterFilters( BOOL bRegister )
+{
+    HRESULT hr = NOERROR;
+    WCHAR achFileName[MAX_PATH];
+    char achTemp[MAX_PATH];
+    ASSERT(g_hInst != 0);
+
+    if( 0 == GetModuleFileNameA(g_hInst, achTemp, sizeof(achTemp))) 
+        return AmHresultFromWin32(GetLastError());
+
+    MultiByteToWideChar(CP_ACP, 0L, achTemp, lstrlenA(achTemp) + 1, 
+                       achFileName, NUMELMS(achFileName));
+  
+    hr = CoInitialize(0);
+    if(bRegister)
+    {
+        hr = AMovieSetupRegisterServer(CLSID_BouncingBall, L"Bouncing Ball", achFileName, L"Both", L"InprocServer32");
+    }
+
+    if( SUCCEEDED(hr) )
+    {
+        IFilterMapper2 *fm = 0;
+        hr = CreateComObject( CLSID_FilterMapper2, IID_IFilterMapper2, fm );
+        if( SUCCEEDED(hr) )
+        {
+            if(bRegister)
+            {
+                IMoniker *pMoniker = 0;
+                REGFILTER2 rf2;
+                rf2.dwVersion = 1;
+                rf2.dwMerit = MERIT_DO_NOT_USE;
+                rf2.cPins = 1;
+                rf2.rgPins = &sudOpPin;
+                hr = fm->RegisterFilter(CLSID_BouncingBall, L"Bouncing Ball", &pMoniker, &CLSID_VideoInputDeviceCategory, NULL, &rf2);
+            }
+            else
+            {
+                hr = fm->UnregisterFilter(&CLSID_VideoInputDeviceCategory, 0, CLSID_BouncingBall);
+            }
+        }
+
+      // release interface
+      //
+      if(fm)
+          fm->Release();
+    }
+
+    if( SUCCEEDED(hr) && !bRegister )
+        hr = AMovieSetupUnregisterServer( CLSID_BouncingBall );
+
+    CoFreeUnusedLibraries();
+    CoUninitialize();
+    return hr;
+}
+
 //
 // DllRegisterServer
 //
@@ -72,7 +131,7 @@ int g_cTemplates = sizeof(g_Templates) / sizeof(g_Templates[0]);
 //
 STDAPI DllRegisterServer()
 {
-    return AMovieDllRegisterServer2(TRUE);
+    return RegisterFilters(TRUE);
 
 } // DllRegisterServer
 
@@ -82,7 +141,7 @@ STDAPI DllRegisterServer()
 //
 STDAPI DllUnregisterServer()
 {
-    return AMovieDllRegisterServer2(FALSE);
+    return RegisterFilters(FALSE);
 
 } // DllUnregisterServer
 
