3cf8b2ea65f9238ba329d1478f0138e8f8d39aa0
TimSC <t.sheerman-chase@surrey.ac.uk>
2013-12-16 18:55:24 +0000

    Change randomisation and make faster

--- videooutfile.py
+++ videooutfile.py
@@ -12,14 +12,13 @@ if __name__=="__main__":
 	img = np.zeros(shape=(imgLen,), dtype=np.uint8)
 
 	for frNum in range(1000):
-		for i in range(imgLen):
-			if (i % 500) > 250:
-				img[i] = np.random.randint(0, 255)
-			else:
-				img[i] = 128
+		img = np.random.randint(0, 255, size=(imgLen,))
+		#for i in range(imgLen):
+		#	if (i % 500) <= 250:
+		#		img[i] = 128
 		
 		print "Frame", frNum
 		outManager.send_frame("test.wmv", str(img.tostring()), "RGB24", 640, 480)
 
-		time.sleep(0.1)
+		time.sleep(0.01)
                 
